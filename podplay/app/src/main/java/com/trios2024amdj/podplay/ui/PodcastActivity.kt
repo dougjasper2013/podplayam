b54f8f39151348dcbc3b3e08ffc858a0d316b92f
Douglas Jasper <doug.jasper@trios.com>
2024-09-03 08:23:36 -0400

    Initial Commit

---
+++ podplay/app/src/main/java/com/trios2024amdj/podplay/ui/PodcastActivity.kt
@@ -0,0 +1,21 @@
+package com.trios2024amdj.podplay.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.trios2024amdj.podplay.R
+
+class PodcastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_podcast)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
