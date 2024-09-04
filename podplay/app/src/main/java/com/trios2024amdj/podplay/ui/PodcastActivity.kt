3aad959ffd848fae84d3d356d622964e7ea6673b
Douglas Jasper <doug.jasper@trios.com>
2024-09-04 08:26:16 -0400

    Added Search Bar

--- podplay/app/src/main/java/com/trios2024amdj/podplay/ui/PodcastActivity.kt
+++ podplay/app/src/main/java/com/trios2024amdj/podplay/ui/PodcastActivity.kt
@@ -1,9 +1,13 @@
 package com.trios2024amdj.podplay.ui
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.trios2024amdj.podplay.R
@@ -28,5 +32,21 @@ class PodcastActivity : AppCompatActivity() {
             Log.i(TAG, "Results = ${results.body()}")
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // 1
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+        // 2
+        val searchMenuItem = menu.findItem(R.id.search_item)
+        val searchView = searchMenuItem?.actionView as SearchView
+        // 3
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        // 4
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        return true
+    }
+
 
 }
