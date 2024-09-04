1affd1a1dd930f2aec8ee80a223659ed1f7a09b6
Douglas Jasper <doug.jasper@trios.com>
2024-09-04 08:46:12 -0400

    Generated Search Results to Logcat

--- podplay/app/src/main/java/com/trios2024amdj/podplay/ui/PodcastActivity.kt
+++ podplay/app/src/main/java/com/trios2024amdj/podplay/ui/PodcastActivity.kt
@@ -2,6 +2,7 @@ package com.trios2024amdj.podplay.ui
 
 import android.app.SearchManager
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
@@ -48,5 +49,27 @@ class PodcastActivity : AppCompatActivity() {
         return true
     }
 
+    private fun performSearch(term: String) {
+        val itunesService = ItunesService.instance
+        val itunesRepo = ItunesRepo(itunesService)
+
+        GlobalScope.launch {
+            val results = itunesRepo.searchByTerm(term)
+            Log.i(TAG, "Results = ${results.body()}")
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY) ?: return
+            performSearch(query)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
+    }
 
 }
